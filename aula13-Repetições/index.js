4faa85cb50875caaddb5ab14cb5e7e62711325e7
FeehZera <119156549+FeehZera@users.noreply.github.com>
2023-01-18 00:11:34 -0300

    att

---
+++ aula13-Repetições/index.js
@@ -0,0 +1,13 @@
+var c = 1
+while (c <= 300) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+/*
+console.log('TUDO BEM?')
+console.log('TUDO BEM?')
+console.log('TUDO BEM?')
+console.log('TUDO BEM?')
+console.log('TUDO BEM?')
+*/
---
+++ aula13-Repetições/index3.js
@@ -0,0 +1,4 @@
+var c = 1
+for (var c;c <=10000000;c++)(
+    console.log(`Olá ${c}`)
+)
