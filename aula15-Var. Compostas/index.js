a1f63f3d757056ac1adee3f19ae9a889a061d0b5
FeehZera <119156549+FeehZera@users.noreply.github.com>
2023-01-19 06:42:33 -0300

    aaaaaaaaaaaaaaaaaaaaaaaa

---
+++ aula15-Var. Compostas/index.js
@@ -0,0 +1,7 @@
+let num = [8, 3, 5, 7, 2]
+
+num.sort()
+num.push(1)
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é: ${num[0]}`)
---
+++ aula15-Var. Compostas/index2.js
@@ -0,0 +1,6 @@
+let num = [8, 3, 5, 7, 2  ,4,7,2,1,7,0,7,3,4,5,9]
+
+for (let pos = 0 ; pos < num.length; pos++) {
+    console.log(`A posição ${pos} tem o valor: ${num[pos]}`)
+}
+console.log('---------------------------------------------------')
