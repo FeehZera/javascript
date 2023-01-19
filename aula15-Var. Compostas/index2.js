ea14d247153deb27f2939911b25422de088e94be
FeehZera <119156549+FeehZera@users.noreply.github.com>
2023-01-19 15:28:34 -0300

    q

--- aula15-Var. Compostas/index2.js
+++ aula15-Var. Compostas/index2.js
@@ -1,6 +1,15 @@
-let num = [8, 3, 5, 7, 2  ,4,7,2,1,7,0,7,3,4,5,9]
+let num = [8, 3, 5, 7, 2]
 
+/*
 for (let pos = 0 ; pos < num.length; pos++) {
     console.log(`A posição ${pos} tem o valor: ${num[pos]}`)
 }
+*/
 console.log('---------------------------------------------------')
+
+//for, Versdao amis simplificado para js
+
+for (let pos in num) {
+    console.log(`A posição ${pos} tem o valor: ${num[pos]}`)
+}
+
