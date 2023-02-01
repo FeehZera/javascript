f91517e4f18127c88c0ee82c9cad8a2681c44cdb
FeehZera <119156549+FeehZera@users.noreply.github.com>
2023-02-01 17:56:19 -0300

    att

--- ex01/index.js
+++ ex01/index.js
@@ -1,21 +1,27 @@
 function carregar(){
+
     var msg = document.getElementById('box1')
     var img = document.getElementById('abc1')  
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 00
+    var hora = data.getHours()
+    
     box1.innerHTML = `agora são ${hora} horas.`
-           if (hora >= 0 && hora < 8 ){
+
+    if (hora >= 0 && hora < 8 ){
         //boa madrugada
         img.src =  'imagens/madrugada.jpg'
+        document.body.style.background = 'black'
     } else if (hora >= 8 && hora < 12) {
         //bom dia
         img.src =  'imagens/manha.jpg'
+        document.body.style.background = 'lightsalmon'
     } else if (hora >= 12 && hora < 18 ){
         //boa tarde
         img.src = 'imagens/tarde.jpg'
+        document.body.style.background = 'burlywood'
     } else {
         //boa noite
         img.src = 'imagens/noite.jpg'
+        document.body.style.background = 'darkgray'
     }
 }
