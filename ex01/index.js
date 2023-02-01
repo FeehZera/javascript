7bac0c9a246055ba00426ab3827cfb330a0d198e
FeehZera <119156549+FeehZera@users.noreply.github.com>
2023-02-01 02:35:48 -0300

    JS é incrivel!!!!!!!!!!!!

--- ex01/index.js
+++ ex01/index.js
@@ -3,16 +3,19 @@ function carregar(){
     var img = document.getElementById('abc1')  
     var data = new Date()
     //var hora = data.getHours()
-    var hora = 15
+    var hora = 00
     box1.innerHTML = `agora são ${hora} horas.`
-    if (hora >= 0 && hora < 12) {
+           if (hora >= 0 && hora < 8 ){
+        //boa madrugada
+        img.src =  'imagens/madrugada.jpg'
+    } else if (hora >= 8 && hora < 12) {
         //bom dia
-        img.src =  'manha.jpg'
-    } else if(hora >= 12 && hora < 18 ){
+        img.src =  'imagens/manha.jpg'
+    } else if (hora >= 12 && hora < 18 ){
         //boa tarde
-        img.src = 'tarde.jpg'
+        img.src = 'imagens/tarde.jpg'
     } else {
         //boa noite
-        img.src = 'noite.jpg'
+        img.src = 'imagens/noite.jpg'
     }
 }
